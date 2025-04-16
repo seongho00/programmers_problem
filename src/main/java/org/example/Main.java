58e94e467898f4f2388aa9aa233f7aa02662df2b
seongho00 <tjdwns000927@gmail.com>
2025-04-16 12:31:04 +0900

    진료순서 정하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,50 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Solution {
+    public List solution(int[] emergency) {
+
+        List<Integer> num = Arrays
+                .stream(emergency)
+                .boxed()
+                .collect(Collectors.toList());
+        List<Integer> rs = new ArrayList<>();
+
+        for (int i = 0; i < num.size(); i++) {
+            rs.add(0);
+        }
+
+        int index = 0;
+        while (true) {
+            for (int i = 0; i < num.size(); i++) {
+                if (Collections.max(num) == -1){
+                    break;
+                }
+                if (Collections.max(num).equals(num.get(i))) {
+                    num.set(i, -1);
+                    rs.set(i,++index);
+                }
+            }
+
+            if (index == num.size()) {
+                break;
+            }
+        }
+
+
+        return rs;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(999));
-    }
-}
-
-class Solution {
-    public int solution(int hp) {
-        return  hp / 5 + (hp % 5) / 3  + ((hp % 5) % 3);
+        System.out.println(solution.solution(new int[]{1, 2, 3, 4, 5, 6, 7}));
     }
 }
