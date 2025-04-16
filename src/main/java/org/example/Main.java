3bf1b33c0916e014f574905e0f65567220267e91
seongho00 <tjdwns000927@gmail.com>
2025-04-16 09:14:34 +0900

    순서쌍의 개수

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,22 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        Solution solution = new Solution();
 
+        System.out.println(solution.solution(100));
+    }
+}
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                answer += 1;
+            }
+        }
+
+        return answer;
     }
 }
