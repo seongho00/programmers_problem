969eed922944c858ec3a1345043d5f5425fff6da
seongho00 <tjdwns000927@gmail.com>
2025-04-16 13:21:36 +0900

    모스부호(1)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,43 +1,49 @@
 package org.example;
 
+
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
 
 class Solution {
-    public List solution(int[] emergency) {
+    public String solution(String letter) {
+        HashMap<String,String> morse = new  HashMap<>();
+        morse.put(".-","a");
+        morse.put("-...","b");
+        morse.put("-.-.","c");
+        morse.put("-..","d");
+        morse.put(".","e");
+        morse.put("..-.","f");
+        morse.put("--.","g");
+        morse.put("....","h");
+        morse.put("..","i");
+        morse.put(".---", "j");
+        morse.put("-.-", "k");
+        morse.put(".-..","l");
+        morse.put("--","m");
+        morse.put("-.", "n");
+        morse.put("---", "o");
+        morse.put(".--.", "p");
+        morse.put("--.-", "q");
+        morse.put(".-.", "r");
+        morse.put("...", "s");
+        morse.put("-", "t");
+        morse.put("..-", "u");
+        morse.put("...-", "v");
+        morse.put(".--", "w");
+        morse.put("-..-", "x");
+        morse.put("-.--", "y");
+        morse.put("--..", "z");
 
-        List<Integer> num = Arrays
-                .stream(emergency)
-                .boxed()
-                .collect(Collectors.toList());
-        List<Integer> rs = new ArrayList<>();
+        List<String> letterBits = new ArrayList<>(Arrays.asList(letter.split(" ")));
+        String result = "";
 
-        for (int i = 0; i < num.size(); i++) {
-            rs.add(0);
+        for (int i = 0; i < letterBits.size(); i++) {
+            result += morse.get(letterBits.get(i));
         }
 
-        int index = 0;
-        while (true) {
-            for (int i = 0; i < num.size(); i++) {
-                if (Collections.max(num) == -1){
-                    break;
-                }
-                if (Collections.max(num).equals(num.get(i))) {
-                    num.set(i, -1);
-                    rs.set(i,++index);
-                }
-            }
-
-            if (index == num.size()) {
-                break;
-            }
-        }
-
-
-        return rs;
+        return result;
     }
 }
 
@@ -45,6 +51,8 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{1, 2, 3, 4, 5, 6, 7}));
+        System.out.println(solution.solution(".--. -.-- - .... --- -."));
     }
 }
+
+
