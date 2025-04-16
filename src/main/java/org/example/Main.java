8ba46da49f117447bb12ea8beb4e5d6761d15230
seongho00 <tjdwns000927@gmail.com>
2025-04-16 16:50:35 +0900

    구슬을 나누는 경우의 수

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,18 +7,19 @@ import java.util.HashMap;
 import java.util.List;
 
 class Solution {
-    public String solution(String rsp) {
-        List<String> rspBits = new ArrayList<>(Arrays.asList(rsp.split("")));
-        String rs = "";
-        for (String r : rspBits) {
-            if (r.startsWith("0")) {
-                rs += "5";
-            } else if(r.startsWith("2")) {
-                rs += "0";
-            } else {
-                rs += "2";
-            }
+    public long solution(int balls, int share) {
+        return factorial(balls , share) / factorial(share, share);
+    }
+    static long factorial(int number, int count) {
+        long rs = 1;
+
+        while(count > 0){
+            int i = number;
+            rs *= i;
+            count --;
+            number--;
         }
+
         return rs;
     }
 }
@@ -27,7 +28,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution("205"));
+        System.out.println(solution.solution(20,3));
     }
 }
 
