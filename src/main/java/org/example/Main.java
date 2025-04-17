ecf21df96e5f3dbf72915c628e4d8479589821a3
seongho00 <tjdwns000927@gmail.com>
2025-04-17 10:24:30 +0900

    배열 회전시키기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,18 +7,22 @@ import java.util.HashMap;
 import java.util.List;
 
 class Solution {
-    public int solution(int[] dot) {
-        if (dot[0] > 0 && dot[1] > 0) {
-            return 1;
+    public int[] solution(int[] numbers, String direction) {
+        if (direction.equals("left")) {
+            int last_number = numbers[0];
+            for (int i = 0; i < numbers.length - 1; i++) {
+                numbers[i] = numbers[i + 1];
+            }
+            numbers[numbers.length - 1] =last_number;
+        } else {
+            int first_number = numbers[numbers.length - 1];
+            for (int i = numbers.length - 1; i >= 1; i--) {
+                numbers[i] = numbers[i - 1];
+            }
+            numbers[0] = first_number;
 
-        } else if (dot[0] > 0 && dot[1] < 0) {
-            return 4;
-        } else if (dot[0] < 0 && dot[1] > 0) {
-            return 2;
-        } else if (dot[0] < 0 && dot[1] < 0) {
-            return 3;
         }
-        return 0;
+        return numbers;
     }
 }
 
@@ -26,7 +30,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{2, 4}));
+        System.out.println(solution.solution(new int[]{4, 455, 6, 4, -1, 45, 6}, "left"));
     }
 }
 
