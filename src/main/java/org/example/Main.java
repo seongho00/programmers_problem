195d43080375b70e5401b8a56c84431d31ea7eb6
seongho00 <tjdwns000927@gmail.com>
2025-04-17 13:23:45 +0900

    팩토리얼

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,16 +7,24 @@ import java.util.HashMap;
 import java.util.List;
 
 class Solution {
-    public int solution(int[] numbers) {
-        int max = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = i + 1 ; j < numbers.length; j++) {
-                if (max < numbers[j] * numbers[i]) {
-                    max = numbers[j] * numbers[i];
-                }
+    public int solution(int n) {
+        int count = 1;
+        while (true) {
+
+            if (factorial(count) <= n) {
+                count++;
+                continue;
             }
+            break;
         }
-        return max;
+        return count - 1;
+    }
+
+    private int factorial(int number) {
+        if (number == 1) {
+            return 1;
+        }
+        return number * factorial(number - 1);
     }
 }
 
@@ -24,7 +32,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{0, 31, 24, 10, 1, 9}));
+        System.out.println(solution.solution(3628800));
     }
 }
 
