419eb8dac3775887c1d9191b23ba1b760544b47f
seongho00 <tjdwns000927@gmail.com>
2025-04-17 09:55:29 +0900

    점의 위치 구하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,9 +7,18 @@ import java.util.HashMap;
 import java.util.List;
 
 class Solution {
-    public int solution(int[] numbers, int k) {
-        int index = (((k - 1) * 2) % numbers.length);
-        return numbers[index];
+    public int solution(int[] dot) {
+        if (dot[0] > 0 && dot[1] > 0) {
+            return 1;
+
+        } else if (dot[0] > 0 && dot[1] < 0) {
+            return 4;
+        } else if (dot[0] < 0 && dot[1] > 0) {
+            return 2;
+        } else if (dot[0] < 0 && dot[1] < 0) {
+            return 3;
+        }
+        return 0;
     }
 }
 
@@ -17,7 +26,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{1, 2, 3}, 3));
+        System.out.println(solution.solution(new int[]{2, 4}));
     }
 }
 
