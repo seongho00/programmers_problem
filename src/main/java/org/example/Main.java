2bc547c9eb2de91fd63c9db271cf4f5bbcccde82
seongho00 <tjdwns000927@gmail.com>
2025-04-17 16:07:33 +0900

    주사위 게임 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,46 +6,16 @@ import java.util.Arrays;
 import java.util.List;
 
 class Solution {
-    public String solution(String code) {
-        List<String> codeBits = new ArrayList<>(Arrays.asList(code.split("")));
-        List<String> ret = new ArrayList<>();
-
-        int mode = 0;
-        for (int idx = 0; idx < codeBits.size(); idx++) {
-            // mode 0 일 때
-            if (mode == 0) {
-                // 모드 바꾸기
-                if (codeBits.get(idx).equals("1")) {
-                    mode = 1;
-                    continue;
-                }
-
-                // 1이 아닐 때 : idx 짝수일 때만 문자 넣기
-                if (idx % 2 == 0){
-                    ret.add(codeBits.get(idx));
-                }
-            }
-
-            // mode 1 일 때
-            if (mode == 1) {
-                // 모드 바꾸기
-                if (codeBits.get(idx).equals("1")) {
-                    mode = 0;
-                    continue;
-                }
-
-                // 1이 아닐 때 : idx 홀수일 때만 문자 넣기
-                if (idx % 2 == 1){
-                    ret.add(codeBits.get(idx));
-                }
-            }
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+        // 세 숫자 모두 다를 때
+        if (a != b && a != c && b != c) {
+            return a + b + c;
+        } else if (a == b && a == c) { // 세 숫자 모두 같을 때
+            return (3 * a) * ((int) (Math.pow(a, 2)) * 3) * ((int) (Math.pow(a, 3)) * 3);
+        } else { // 나머지 경우
+            return (a + b + c) * ((int) (Math.pow(a, 2)) + (int) (Math.pow(b, 2) + Math.pow(c, 2)));
         }
-        // ret 에 아무것도 없을 때
-        if (ret.isEmpty()){
-            return "EMPTY";
-        }
-
-        return String.join("", ret);
     }
 }
 
@@ -53,7 +23,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution("abc1abc1abc"));
+        System.out.println(solution.solution(4, 4, 4));
     }
 }
 
