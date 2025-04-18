02a6234f1a8cf4132725622dcecf6db15bfe0e58
seongho00 <tjdwns000927@gmail.com>
2025-04-18 13:43:54 +0900

    컨트롤 제트

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,31 +3,26 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.stream.Collectors;
+import java.util.List;
+import java.util.Arrays;
 
 class Solution {
-    public List<Integer> solution(int n) {
+    public int solution(String s) {
+        int answer = 0;
+        //입력받은 문자 공백 기준으로 배열에 담기
+        List<String> strings = Arrays.asList(s.split(" "));
 
-        List<Integer> numbers = new ArrayList<>();
-        int i = 2;
-        // 입력받은 정수까지 반복
-        while (i <= n) {
-            // 나눠지는 수가 있을 때 = 없을 때 까지 반복
-            if (n % i == 0) {
-                n = n / i;
-                numbers.add(i);
-            } else {
-            // 나눠지는 수가 없으면 다음 수로 넘어감
-                i++;
+        for (int i = 0; i < strings.size(); i++) {
+            // Z가 나오면
+            if (strings.get(i).equals("Z")) {
+                answer -= Integer.parseInt(strings.get(i - 1));
+            } else { // Z가 아닌 숫자면
+                answer += Integer.parseInt(strings.get(i));
             }
-
         }
-        // 중복된 값 제거
-        numbers = numbers.stream().distinct().collect(Collectors.toList());
-        // 오림차순 정렬
-        Collections.sort(numbers);
-        return numbers;
+
+        return answer;
     }
 }
 
@@ -35,7 +30,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(420));
+        System.out.println(solution.solution("10 20 30 40"));
     }
 }
 
