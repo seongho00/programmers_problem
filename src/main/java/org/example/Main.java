ad31da8aec2dc922d77a82116b8167590a43e674
seongho00 <tjdwns000927@gmail.com>
2025-04-18 13:39:31 +0900

    소인수분해

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,33 @@
 package org.example;
 
 
-import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class Solution {
-    public int solution(String my_string) {
-        int answer = 0;
+    public List<Integer> solution(int n) {
 
-        // 문자 모두 소문자로
-        my_string = my_string.toLowerCase();
-        // 숫자 제외 문자 제거
-        my_string = my_string.replaceAll("[a-z]", "");
-        // 숫자 합 구하기
-        for (int i = 0; i < my_string.length(); i++) {
-            answer += Integer.parseInt(my_string.split("")[i]);
+        List<Integer> numbers = new ArrayList<>();
+        int i = 2;
+        // 입력받은 정수까지 반복
+        while (i <= n) {
+            // 나눠지는 수가 있을 때 = 없을 때 까지 반복
+            if (n % i == 0) {
+                n = n / i;
+                numbers.add(i);
+            } else {
+            // 나눠지는 수가 없으면 다음 수로 넘어감
+                i++;
+            }
+
         }
-
-        return answer;
+        // 중복된 값 제거
+        numbers = numbers.stream().distinct().collect(Collectors.toList());
+        // 오림차순 정렬
+        Collections.sort(numbers);
+        return numbers;
     }
 }
 
@@ -27,7 +35,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution("1a2b3c4d123"));
+        System.out.println(solution.solution(420));
     }
 }
 
