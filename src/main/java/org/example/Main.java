9bce20141f319b67ff006f8e95b5bfd4918b4c37
seongho00 <tjdwns000927@gmail.com>
2025-04-18 13:47:32 +0900

    배열 원소의 길이

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,27 +2,19 @@ package org.example;
 
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.stream.Collectors;
 import java.util.List;
-import java.util.Arrays;
 
 class Solution {
-    public int solution(String s) {
-        int answer = 0;
-        //입력받은 문자 공백 기준으로 배열에 담기
-        List<String> strings = Arrays.asList(s.split(" "));
+    public List<Integer> solution(String[] strList) {
+        // 길이 담을 배열 생성
+        List<Integer> wordLength = new ArrayList<>();
 
-        for (int i = 0; i < strings.size(); i++) {
-            // Z가 나오면
-            if (strings.get(i).equals("Z")) {
-                answer -= Integer.parseInt(strings.get(i - 1));
-            } else { // Z가 아닌 숫자면
-                answer += Integer.parseInt(strings.get(i));
-            }
+        for (int i = 0; i < strList.length; i++) {
+            // 각 문자 길이 배열에 넣기
+            wordLength.add(strList[i].length());
         }
 
-        return answer;
+        return wordLength;
     }
 }
 
@@ -30,7 +22,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution("10 20 30 40"));
+        System.out.println(solution.solution(new String[]{"We", "are", "the", "world!"}));
     }
 }
 
