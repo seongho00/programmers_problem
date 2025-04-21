d3ec66e007fd47adc114870b6a73dc5ca79d480c
seongho00 <tjdwns000927@gmail.com>
2025-04-21 14:34:13 +0900

    가까운수

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,33 @@
 package org.example;
 
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 class Solution {
-    public String solution(String my_string) {
-        String answer = "";
-        
-        for (int i = 0; i < my_string.length(); i++) {
-            if (Character.isUpperCase(my_string.charAt(i))) {
-                answer += (my_string.charAt(i) + "").toLowerCase();
-            } else {
-                answer += (my_string.charAt(i) + "").toUpperCase();
+    public int solution(int[] array, int n) {
+        int answer = 0;
+
+        List<Integer> differences = new ArrayList<>();
+        List<Integer> answers = new ArrayList<>();
+        // 차이값 구해서 넣기 array 와 n의 차이값 구해서 넣기(절대값으로)
+        for (int i = 0; i < array.length; i++) {
+            differences.add(Math.abs(array[i] - n));
+        }
+        // 차이값의 최소 구하기
+        int min = Collections.min(differences);
+
+        // 차이값의 최소랑 같은 array 배열의 숫자 찾아서 answers 배열에 넣기
+        for (int i = 0; i< array.length; i++) {
+            if (Math.abs(array[i] - n) == min) {
+                answers.add(array[i]);
             }
         }
-        return answer;
+        // answers 배열 중 가장 작은 값 출력
+        return Collections.min(answers);
     }
 }
 
@@ -25,7 +35,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution("cccCCC"));
+        System.out.println(solution.solution(new int[]{10, 12}, 11));
     }
 }
 
