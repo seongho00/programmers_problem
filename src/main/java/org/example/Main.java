1c2c9ed6aa043e5aca1383a02b83f00cd762c907
seongho00 <tjdwns000927@gmail.com>
2025-04-22 15:24:45 +0900

    편지

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,28 +7,8 @@ import java.util.Collections;
 import java.util.List;
 
 class Solution {
-    public String solution(String s) {
-        String answer = "";
-        List<String> list = Arrays.asList(s.split(""));
-
-        for (int i = 0; i < list.size(); i++) {
-            boolean isMulti = false;
-            if (list.get(i).equals("")) {
-                continue;
-            }
-            for (int j = i + 1; j < list.size(); j++) {
-                if (list.get(i).equals(list.get(j))) {
-                    list.set(j, "");
-                    isMulti = true;
-                }
-            }
-            if (isMulti) {
-                list.set(i, "");
-            }
-        }
-        Collections.sort(list);
-
-        return String.join("",list).trim();
+    public int solution(String message) {
+        return message.length() * 2;
     }
 }
 
@@ -36,7 +16,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution("hello"));
+        System.out.println(solution.solution("I love you~"));
     }
 }
 
