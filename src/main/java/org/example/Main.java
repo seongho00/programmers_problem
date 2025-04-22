5ffa5dd3a6513179c4dc9109c4bd71e1e70402d9
seongho00 <tjdwns000927@gmail.com>
2025-04-22 15:22:31 +0900

    한 번만 등장하는 문자

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,33 @@ package org.example;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 class Solution {
-    public List<Integer> solution(int n) {
-        List<Integer> measures = new ArrayList<>();
+    public String solution(String s) {
+        String answer = "";
+        List<String> list = Arrays.asList(s.split(""));
 
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0) {
-                measures.add(i);
+        for (int i = 0; i < list.size(); i++) {
+            boolean isMulti = false;
+            if (list.get(i).equals("")) {
+                continue;
+            }
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i).equals(list.get(j))) {
+                    list.set(j, "");
+                    isMulti = true;
+                }
+            }
+            if (isMulti) {
+                list.set(i, "");
             }
         }
-        return measures;
+        Collections.sort(list);
+
+        return String.join("",list).trim();
     }
 }
 
@@ -21,7 +36,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(29));
+        System.out.println(solution.solution("hello"));
     }
 }
 
