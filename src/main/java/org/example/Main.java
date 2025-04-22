da6a42758cec21e3d9c5c3da940a8c0cb759d7a5
seongho00 <tjdwns000927@gmail.com>
2025-04-22 16:37:39 +0900

    수 조작하기 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,18 +5,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    public List<Integer> solution(int[] num_list) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < num_list.length; i++) {
-            list.add(num_list[i]);
-        }
-        if (list.get(list.size() - 1) > list.get(list.size() - 2)) {
-            list.add(list.get(list.size() - 1) - list.get(list.size() - 2));
-        } else {
-            list.add(list.get(list.size() - 1) * 2);
-        }
+    public int solution(int n, String control) {
 
-        return list;
+        for (int i = 0; i < control.length(); i++) {
+            if (control.charAt(i) == 'w') {
+                n += 1;
+            } else if (control.charAt(i) == 'a') {
+                n -= 10;
+            } else if (control.charAt(i) == 'd') {
+                n += 10;
+            } else if (control.charAt(i) == 's') {
+                n -= 1;
+            }
+        }
+        return n;
     }
 }
 
@@ -24,7 +26,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{5, 2, 1, 7, 5}));
+        System.out.println(solution.solution(0, "wsdawsdassw"));
 
     }
 }
