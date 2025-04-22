85f275a545073283ddc10ac9b46b398d90422fd3
seongho00 <tjdwns000927@gmail.com>
2025-04-22 15:36:21 +0900

    문자열 계산하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,21 +3,21 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 class Solution {
-    public List<Integer> solution(int[] array) {
-        List<Integer> list = new ArrayList<>();
-        List<Integer> answer = new ArrayList<>();
-        for (int i = 0; i < array.length; i++) {
-            list.add(array[i]);
-        }
-        answer.add(Collections.max(list));
+    public int solution(String my_string) {
+        int answer = 0;
 
-        for (int i = 0; i < list.size(); i++) {
-            if (answer.get(0) == list.get(i)) {
-                answer.add(i);
-            }
+        if (my_string.contains(" - ")) {
+            my_string = my_string.replace(" - ", " + -");
+        }
+
+        List<String> my_strings = Arrays.asList(my_string.split(" \\+ "));
+
+        for  (int i = 0; i < my_strings.size(); i++) {
+            answer += Integer.parseInt(my_strings.get(i));
         }
 
         return answer;
@@ -28,7 +28,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{1, 8, 3}));
+        System.out.println(solution.solution("3 + 4"));
     }
 }
 
