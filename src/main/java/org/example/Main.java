96bc2079b23dd848dc6c24f4fc31b2c4b9340a0e
seongho00 <tjdwns000927@gmail.com>
2025-04-22 14:16:01 +0900

    약수 구하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,31 +1,19 @@
 package org.example;
 
 
-import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    public int solution(String[] babbling) {
-        int answer = 0;
-        List<String> speakWords = new ArrayList<>();
-        speakWords.add("aya");
-        speakWords.add("ye");
-        speakWords.add("woo");
-        speakWords.add("ma");
+    public List<Integer> solution(int n) {
+        List<Integer> measures = new ArrayList<>();
 
-        int count = 0;
-        for (int i = 0; i < babbling.length; i++) {
-            for (int j = 0; j < speakWords.size(); j++) {
-                babbling[i] = babbling[i].replaceAll(speakWords.get(j), " ");
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                measures.add(i);
             }
         }
-        for (int i = 0; i < babbling.length; i++) {
-            if(babbling[i].trim().isEmpty()){
-                count++;
-            }
-        }
-        return count;
+        return measures;
     }
 }
 
@@ -33,7 +21,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new String[]{"ayaye", "uuuma", "ye", "yemawoo", "ayaa"}));
+        System.out.println(solution.solution(29));
     }
 }
 
