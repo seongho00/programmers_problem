273cd4cb95bc1443f9a50a1fb42413b604e9c0a4
seongho00 <tjdwns000927@gmail.com>
2025-04-23 09:15:12 +0900

    n의 배수 고르기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,21 @@
 package org.example;
 
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    public int solution(int num, int k) {
-        List<String> list = Arrays.asList((num + "").split(""));
-        String q = k +"";
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).equals(q)) {
-                return i + 1;
+    public List<Integer> solution(int n, int[] numList) {
+        List<Integer> answer = new ArrayList<>();
+        
+        for (int i = 0; i < numList.length; i++) {
+            if (numList[i] % n == 0) {
+                answer.add(numList[i]);
             }
         }
 
-        return -1;
+        return answer;
     }
 }
 
@@ -23,7 +23,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(232443	, 4));
+        System.out.println(solution.solution(3, new int[]{4, 5, 6, 7, 8, 9, 10, 11, 12}));
 
     }
 }
