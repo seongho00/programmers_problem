f48c72b2d0af2ae94059b56bdb22c074d0669a0c
seongho00 <tjdwns000927@gmail.com>
2025-04-24 10:29:22 +0900

    OX퀴즈

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,13 +6,30 @@ import java.util.Arrays;
 import java.util.List;
 
 class Solution {
-    public int solution(int n) {
-        int answer = 0;
-        List<String> numbers = Arrays.asList(String.valueOf(n).split(""));
-        for (int i = 0; i < numbers.size(); i++) {
-            answer += Integer.parseInt(numbers.get(i));
+    public List<String> solution(String[] quiz) {
+        List<String> isRight = new ArrayList<>();
+
+        for (int i = 0; i < quiz.length; i++) {
+            String quizString = quiz[i].split(" = ")[0];
+            String answer = quiz[i].split(" = ")[1];
+            int quizAnswer = 0;
+            if (quizString.contains(" - ")) {
+                quizString = quizString.replace(" - ", " + -");
+            }
+            List<String> numbers = new ArrayList<>(Arrays.asList(quizString.split(" \\+ ")));
+
+            for (int j = 0; j < numbers.size(); j++) {
+                quizAnswer += Integer.parseInt(numbers.get(j));
+            }
+
+            if (quizAnswer == Integer.parseInt(answer)) {
+                isRight.add("O");
+            } else {
+                isRight.add("X");
+            }
+
         }
-        return answer;
+        return isRight;
     }
 }
 
@@ -20,7 +37,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(930211));
+        System.out.println(solution.solution(new String[]{"19 + -6 + 5 - 4 + 5 + 4 = 13", "5 + 66 = 71", "5 - 15 = 63", "3 - 1 = 2"}));
 
     }
 }
