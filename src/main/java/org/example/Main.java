bf750fd3deefe6ced0a1d5297aa1019fff2e55ea
seongho00 <tjdwns000927@gmail.com>
2025-04-28 11:42:34 +0900

    7의 개수

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,25 @@ package org.example;
 
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 class Solution {
-    public int solution(int[] array, int height) {
+    public int solution(int[] array) {
         int answer = 0;
+
         for (int i = 0; i < array.length; i++) {
-            if (array[i] > height) {
-                answer++;
+            String number = array[i] + "";
+            List<String> numberSplit = Arrays.asList(number.split(""));
+            for (int j = 0; j < numberSplit.size(); j++) {
+                if (numberSplit.get(j).equals("7")) {
+                    answer++;
+                }
             }
         }
+
+
         return answer;
     }
 }
@@ -22,7 +29,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{149, 180, 192, 170}, 167));
+        System.out.println(solution.solution(new int[]{7, 77, 17}));
 
     }
 }
