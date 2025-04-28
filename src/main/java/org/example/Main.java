eb2ccca74847c931ac0f5d8253b66be5dec102e0
seongho00 <tjdwns000927@gmail.com>
2025-04-28 11:36:03 +0900

    머쓱이보다 키 큰 사람

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,10 +7,10 @@ import java.util.Collections;
 import java.util.List;
 
 class Solution {
-    public int solution(int[] array, int n) {
+    public int solution(int[] array, int height) {
         int answer = 0;
         for (int i = 0; i < array.length; i++) {
-            if (array[i] == n) {
+            if (array[i] > height) {
                 answer++;
             }
         }
@@ -22,7 +22,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{1, 1, 2, 3, 4, 5}, 1));
+        System.out.println(solution.solution(new int[]{149, 180, 192, 170}, 167));
 
     }
 }
