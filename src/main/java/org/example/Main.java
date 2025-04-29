8c025f7bd8b8557c5844895cdc245f2127dc92ed
seongho00 <tjdwns000927@gmail.com>
2025-04-29 13:07:21 +0900

    다항식 더하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,26 +7,46 @@ import java.util.Arrays;
 import java.util.List;
 
 class Solution {
-    public int solution(int[] numbers) {
-        int max = numbers[0] * numbers[1];
-
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = i + 1; j < numbers.length; j++) {
-                if (numbers[i] * numbers[j] > max) {
-                    max = numbers[i] * numbers[j];
-                }
+    public String solution(String polynomial) {
+        int sum_x = 0;
+        int number = 0;
+        List<String> polynomialBits = Arrays.asList(polynomial.split(" \\+ "));
+        for (int i = 0; i < polynomialBits.size(); i++) {
+            if (polynomialBits.get(i).equals("x")) {
+                polynomialBits.set(i, "1x");
+            }
+            if (polynomialBits.get(i).contains("x")) {
+                String[] bits = polynomialBits.get(i).split("x");
+                sum_x += Integer.parseInt(bits[0]);
+            } else {
+                number += Integer.parseInt(polynomialBits.get(i));
+            }
+        }
+        String result = sum_x + "x + " + number;
+        if (number == 0) {
+            result = sum_x + "x";
+            if (sum_x == 1) {
+                result = "x";
+            }
+        } else if (sum_x == 0) {
+            result = number + "";
+        } else {
+            if (sum_x == 1) {
+                result = "x + " + number;
             }
         }
 
-        return max;
+        return result;
     }
+
+
 }
 
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{1, 2, -3, 4, -5}));
+        System.out.println(solution.solution("1 + 5"));
 
     }
 }
