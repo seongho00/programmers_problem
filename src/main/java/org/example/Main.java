5efe351bebfd1cf575acc37fbb456c84cb422e2a
seongho00 <tjdwns000927@gmail.com>
2025-05-07 09:08:18 +0900

    로그인 성공?

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,50 +3,36 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Arrays;
 
 class Solution {
-    public String solution(String polynomial) {
-        int sum_x = 0;
-        int number = 0;
-        List<String> polynomialBits = Arrays.asList(polynomial.split(" \\+ "));
-        for (int i = 0; i < polynomialBits.size(); i++) {
-            if (polynomialBits.get(i).equals("x")) {
-                polynomialBits.set(i, "1x");
+    public String solution(String[] id_pw, String[][] db) {
+        int id_index = 0;
+        for (int i = 0; i < db.length; i++) {
+            if (id_pw[0].equals(db[i][0])) {
+                id_index = i;
+                break;
             }
-            if (polynomialBits.get(i).contains("x")) {
-                String[] bits = polynomialBits.get(i).split("x");
-                sum_x += Integer.parseInt(bits[0]);
-            } else {
-                number += Integer.parseInt(polynomialBits.get(i));
-            }
-        }
-        String result = sum_x + "x + " + number;
-        if (number == 0) {
-            result = sum_x + "x";
-            if (sum_x == 1) {
-                result = "x";
-            }
-        } else if (sum_x == 0) {
-            result = number + "";
-        } else {
-            if (sum_x == 1) {
-                result = "x + " + number;
+            if (i == db.length - 1) {
+                return "fail";
             }
         }
 
-        return result;
+        if (id_pw[1].equals(db[id_index][1])) {
+            return "login";
+        }
+
+
+        return "wrong pw";
     }
-
-
 }
 
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution("1 + 5"));
+        System.out.println(solution.solution(new String[]{"meosseugi", "1234"}, new String[][]{{"rardss", "123"}, {"yyoom", "1234"}, {"meosseugi", "1234"}}));
 
     }
 }
