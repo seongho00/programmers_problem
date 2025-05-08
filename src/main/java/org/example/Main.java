6e842c1b1e9b60253bcedfd3f42aa8a9f34049d6
seongho00 <tjdwns000927@gmail.com>
2025-05-08 13:42:09 +0900

    카운트 업

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,23 +4,13 @@ package org.example;
 import java.util.*;
 
 class Solution {
-    public List<Integer> solution(int[] arr, int[][] queries) {
+    public List<Integer> solution(int start_num, int end_num) {
         List<Integer> answer = new ArrayList<>();
 
-        for (int i = 0; i < queries.length; i++) {
-            List<Integer> list = new ArrayList<>();
-            for (int j = queries[i][0]; j <= queries[i][1]; j++) {
-                if (arr[j] > queries[i][2]) {
-                    list.add(arr[j]);
-                }
-            }
-            Collections.sort(list);
-            if (list.isEmpty()) {
-                answer.add(-1);
-            } else {
-                answer.add(list.get(0));
-            }
+        for (int i = start_num; i <= end_num; i++) {
+            answer.add(i);
         }
+
         return answer;
     }
 }
@@ -29,7 +19,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{0, 1, 2, 4, 3}, new int[][]{{0, 4, 2}, {0, 3, 2}, {0, 2, 2}}));
+        System.out.println(solution.solution(3, 10));
 
     }
 }
