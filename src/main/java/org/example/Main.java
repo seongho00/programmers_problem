c61b5620a0ae1cdd4b4ad24dcff9b8d45338158b
seongho00 <tjdwns000927@gmail.com>
2025-05-08 13:39:42 +0900

    수열과 구간 쿼리 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,40 +4,23 @@ package org.example;
 import java.util.*;
 
 class Solution {
-    public List<Integer> solution(int[] numList, int n) {
-        List<Integer> numbers = new ArrayList<>();
-        List<Integer> numbers_abs = new ArrayList<>();
+    public List<Integer> solution(int[] arr, int[][] queries) {
         List<Integer> answer = new ArrayList<>();
-        boolean plus;
-        for (int i = 0; i < numList.length; i++) {
-            numbers.add(numList[i] - n);
-            numbers_abs.add(Math.abs(numList[i] - n));
-        }
-        Collections.sort(numbers_abs);
-        for (int i = 0; i < numbers_abs.size(); i++) {
-            plus = false;
-            for (int j = 0; j < numbers.size(); j++) {
-                if (numbers_abs.get(i) == -numbers.get(j) && numbers_abs.get(i) != 0) {
-                    answer.add(-numbers_abs.get(i) + n);
-                    numbers.remove(j);
-                    plus = true;
+
+        for (int i = 0; i < queries.length; i++) {
+            List<Integer> list = new ArrayList<>();
+            for (int j = queries[i][0]; j <= queries[i][1]; j++) {
+                if (arr[j] > queries[i][2]) {
+                    list.add(arr[j]);
                 }
             }
-            if (!plus) {
-                if (i != 0) {
-                    if (Objects.equals(numbers_abs.get(i), numbers_abs.get(i - 1))) {
-                        answer.add(i - 1, numbers_abs.get(i) + n);
-                    } else {
-                        answer.add(numbers_abs.get(i) + n);
-                    }
-                } else {
-                    answer.add(numbers_abs.get(i) + n);
-                }
-
-
+            Collections.sort(list);
+            if (list.isEmpty()) {
+                answer.add(-1);
+            } else {
+                answer.add(list.get(0));
             }
         }
-
         return answer;
     }
 }
@@ -46,7 +29,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{1, 2, 3, 4, 5}, 1));
+        System.out.println(solution.solution(new int[]{0, 1, 2, 4, 3}, new int[][]{{0, 4, 2}, {0, 3, 2}, {0, 2, 2}}));
 
     }
 }
