fe13110d9149b88328a91446057c5cd7071bfe9d
seongho00 <tjdwns000927@gmail.com>
2025-05-09 13:54:11 +0900

    배열 만들기 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,54 +2,30 @@ package org.example;
 
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 
 class Solution {
-    public int solution(int a, int b, int c, int d) {
-        int answer = 0;
-        List<Integer> numbers = new ArrayList<>();
-        numbers.add(a);
-        numbers.add(b);
-        numbers.add(c);
-        numbers.add(d);
-        List<Integer> distinctNumbers = new ArrayList<>(numbers.stream().distinct().collect(Collectors.toList()));
+    public List<Integer> solution(int l, int r) {
+        List<Integer> answer = new ArrayList<>();
+        for (int i = l; i <= r; i++) {
+            boolean isBreak = false;
+            List<String> numbers = new ArrayList<>(Arrays.asList(String.valueOf(i).split("")));
+            for (String number : numbers) {
+                if (!number.equals("0") && !number.equals("5")) {
+                    isBreak = true;
+                    break;
+                }
+            }
 
-        for (Integer number : distinctNumbers) {
-            numbers.remove(number);
+            if (isBreak) {
+                continue;
+            }
+
+            answer.add(i);
         }
-        numbers = numbers.stream().sorted().distinct().collect(Collectors.toList());
-
-        // 숫자가 모두 같을 때
-        if (distinctNumbers.size() == 1) {
-            return 1111 * distinctNumbers.get(0);
-        }
-
-        // 3개의 숫자가 같을 때
-        if (distinctNumbers.size() == 2 && numbers.size() == 1) {
-            int distinctNumber = numbers.get(0) == distinctNumbers.get(0) ? distinctNumbers.get(1) : distinctNumbers.get(0);
-            return (10 * numbers.get(0) + distinctNumber) * (10 * numbers.get(0) + distinctNumber);
-
-        }
-
-        // 두개씩 숫자가 같을 때
-        if (distinctNumbers.size() == 2 && numbers.size() == 2) {
-            int p = numbers.get(0);
-            int q = numbers.get(1);
-            return (p + q) * Math.abs(p - q);
-
-        }
-
-        // 두개만 같은 숫자 일 때 
-        if (distinctNumbers.size() == 3) {
-            distinctNumbers.remove(numbers.get(0));
-            int q = distinctNumbers.get(0);
-            int r = distinctNumbers.get(1);
-            return q * r;
-        }
-        // 모두 다를 때
-        if (distinctNumbers.size() == 4) {
-            return Collections.min(distinctNumbers);
+        Collections.sort(answer);
+        if (answer.isEmpty()) {
+            answer.add(-1);
         }
         return answer;
     }
@@ -59,7 +35,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(1, 2, 4, 3));
+        System.out.println(solution.solution(5, 555));
 
     }
 }
