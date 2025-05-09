bb97ff7d745675bd26707d065fc9c6bfeb41d6f2
seongho00 <tjdwns000927@gmail.com>
2025-05-09 15:25:24 +0900

    약수의 합

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,28 +4,19 @@ package org.example;
 import java.util.*;
 
 
+import java.util.*;
+
 class Solution {
-    public List<Integer> solution(int l, int r) {
-        List<Integer> answer = new ArrayList<>();
-        for (int i = l; i <= r; i++) {
-            boolean isBreak = false;
-            List<String> numbers = new ArrayList<>(Arrays.asList(String.valueOf(i).split("")));
-            for (String number : numbers) {
-                if (!number.equals("0") && !number.equals("5")) {
-                    isBreak = true;
-                    break;
-                }
-            }
-
-            if (isBreak) {
-                continue;
-            }
-
-            answer.add(i);
+    public int solution(int n) {
+        if (n == 0) {
+            return 0;
         }
-        Collections.sort(answer);
-        if (answer.isEmpty()) {
-            answer.add(-1);
+        int answer = 1;
+
+        for (int i = 2; i <= n; i++) {
+            if (n % i == 0) {
+                answer += i;
+            }
         }
         return answer;
     }
@@ -35,7 +26,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(5, 555));
+        System.out.println(solution.solution(6));
 
     }
 }
