88a20dfbf2ed75b9dfcc5fa77b93d831c784b7af
seongho00 <tjdwns000927@gmail.com>
2025-05-09 09:16:03 +0900

    0로 나눈 나머지

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,14 @@ import java.util.*;
 
 
 class Solution {
-    public String solution(String my_string, int[] index_list) {
-        String answer = "";
-        for (int i = 0; i < index_list.length; i++) {
-            answer += my_string.charAt(index_list[i]);
+    public int solution(String number) {
+        int answer = 0;
+        List<String> numberList = new ArrayList<>(Arrays.asList(number.split("")));
+
+        for (int i = 0; i < numberList.size(); i++) {
+            answer += Integer.parseInt(numberList.get(i));
         }
+        answer %= 9;
         return answer;
     }
 }
@@ -18,7 +21,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution("cvsgiorszzzmrpaqpe", new int[]{16, 6, 5, 3, 12, 14, 11, 11, 17, 12, 7}));
+        System.out.println(solution.solution("78720646226947352489"));
 
     }
 }
