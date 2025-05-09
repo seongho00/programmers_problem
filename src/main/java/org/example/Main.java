68daac59ccf14be26e1158e7e1e6c574cc72b9ec
seongho00 <tjdwns000927@gmail.com>
2025-05-09 09:12:31 +0900

    글자 이어 붙여 문자열 만들기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,35 +5,20 @@ import java.util.*;
 
 
 class Solution {
-    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
-        boolean V1 = V(x1, x2);
-        boolean V2 = V(x3, x4);
-        boolean answer = X(V1, V2);
+    public String solution(String my_string, int[] index_list) {
+        String answer = "";
+        for (int i = 0; i < index_list.length; i++) {
+            answer += my_string.charAt(index_list[i]);
+        }
         return answer;
-
     }
-
-    private boolean X(boolean v1, boolean v2) {
-        if (v1 == false || v2 == false) {
-            return false;
-        }
-        return true;
-    }
-
-    private boolean V(boolean b1, boolean b2) {
-        if (b1 == false && b2 == false) {
-            return false;
-        }
-        return true;
-    }
-
 }
 
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(true, false, false, false));
+        System.out.println(solution.solution("cvsgiorszzzmrpaqpe", new int[]{16, 6, 5, 3, 12, 14, 11, 11, 17, 12, 7}));
 
     }
 }
