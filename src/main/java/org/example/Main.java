614141fa9b5a6b422836b86b02717eeb8597577f
seongho00 <tjdwns000927@gmail.com>
2025-05-09 09:09:14 +0900

    간단한 논리 연산

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,29 +5,35 @@ import java.util.*;
 
 
 class Solution {
-    public List<Integer> solution(int[] arr) {
-        List<Integer> stk = new ArrayList<>();
-        int i = 0;
-        while (i < arr.length) {
-            if (stk.isEmpty()) {
-                stk.add(arr[i]);
-                i++;
-            } else if (stk.get(stk.size() - 1) < arr[i]) {
-                stk.add(arr[i]);
-                i++;
-            } else if (stk.get(stk.size() - 1) >= arr[i]) {
-                stk.remove(stk.size() - 1);
-            }
-        }
-        return stk;
+    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+        boolean V1 = V(x1, x2);
+        boolean V2 = V(x3, x4);
+        boolean answer = X(V1, V2);
+        return answer;
+
     }
+
+    private boolean X(boolean v1, boolean v2) {
+        if (v1 == false || v2 == false) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean V(boolean b1, boolean b2) {
+        if (b1 == false && b2 == false) {
+            return false;
+        }
+        return true;
+    }
+
 }
 
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{1, 4, 2, 5, 3}));
+        System.out.println(solution.solution(true, false, false, false));
 
     }
 }
