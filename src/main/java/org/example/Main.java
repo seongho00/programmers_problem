e9f242558c5d89c65763ecb82cbe826fb5e0cfb2
seongho00 <tjdwns000927@gmail.com>
2025-05-09 16:14:12 +0900

    jstl 설정 추가

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,8 @@ import java.util.*;
 
 
 class Solution {
-    public double solution(int[] arr) {
-        double answer = 0;
-        for (int i = 0; i < arr.length; i++) {
-            answer += arr[i];
-        }
-        answer /= arr.length;
+    public long[] solution(int x, int n) {
+        long[] answer = {};
         return answer;
     }
 }
@@ -19,7 +15,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{1, 2, 3, 4}));
+        System.out.println(solution.solution(2, 5));
 
     }
 }
