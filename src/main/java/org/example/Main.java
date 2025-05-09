7f532b0b48f77061ab1c1b59a7ce6e3740482b4e
seongho00 <tjdwns000927@gmail.com>
2025-05-09 16:48:21 +0900

    문자열을 정수로 바꾸기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,9 @@ import java.util.*;
 
 
 class Solution {
-    public int solution(int n) {
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 1) {
-                return i;
-            }
-        }
-        return 0;
+    public int solution(String s) {
+
+        return Integer.parseInt(s);
     }
 }
 
