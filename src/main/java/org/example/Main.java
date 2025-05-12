fc27a5d3ee4aca13a53972a977985222691753be
seongho00 <tjdwns000927@gmail.com>
2025-05-12 09:06:30 +0900

    순서 바꾸기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,22 +3,25 @@ package org.example;
 
 import java.util.*;
 
-
 class Solution {
     public List<Integer> solution(int[] num_list, int n) {
         List<Integer> answer = new ArrayList<>();
-        for (int i = n - 1; i < num_list.length; i++) {
+        for (int i = n; i < num_list.length; i++) {
+            answer.add(num_list[i]);
+        }
+        for (int i = 0; i < n; i++) {
             answer.add(num_list[i]);
         }
         return answer;
     }
 }
 
+
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{2, 1, 6}, 3));
+        System.out.println(solution.solution(new int[]{2, 1, 6}, 1));
 
     }
 }
