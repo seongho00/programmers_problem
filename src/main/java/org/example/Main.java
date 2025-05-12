03b7e2c4128599c757e54401a84791bef4808924
seongho00 <tjdwns000927@gmail.com>
2025-05-12 14:13:03 +0900

    수열과 구간 쿼리 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,15 +4,13 @@ package org.example;
 import java.util.*;
 
 class Solution {
-    public int solution(int[] numbers, int n) {
-        int answer = numbers[0];
-        for (int i = 1; i <= n; i++) {
-            if (answer > n) {
-                return answer;
+    public int[] solution(int[] arr, int[][] queries) {
+        for (int i = 0; i < queries.length; i++) {
+            for (int j = queries[i][0]; j <= queries[i][1]; j++) {
+                arr[j] += 1;
             }
-            answer += numbers[i];
         }
-        return answer;
+        return arr;
     }
 }
 
@@ -20,7 +18,8 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{34, 5, 71, 29, 100, 34}, 123));
+        System.out.println(solution.solution(new int[]{0, 1, 2, 3, 4}, new int[][]{{0, 1}, {1, 2}, {2, 3}
+        }));
 
     }
 }
