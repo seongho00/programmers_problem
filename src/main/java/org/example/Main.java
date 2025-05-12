a493466ff62baf435d1a127c96d4e0c792687ae3
seongho00 <tjdwns000927@gmail.com>
2025-05-12 13:01:55 +0900

    n개 간격의 원소들

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,10 +6,9 @@ import java.util.*;
 class Solution {
     public List<Integer> solution(int[] num_list, int n) {
         List<Integer> answer = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < num_list.length; i += n) {
             answer.add(num_list[i]);
         }
-
         return answer;
     }
 }
@@ -19,7 +18,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{2, 1, 6}, 1));
+        System.out.println(solution.solution(new int[]{4, 2, 6, 1, 7, 6}, 2));
 
     }
 }
