d66422be2e863288ec2e804276296916c0c85aa7
seongho00 <tjdwns000927@gmail.com>
2025-05-13 10:48:41 +0900

    대문자로 바꾸기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,8 @@ package org.example;
 import java.util.Locale;
 
 class Solution {
-    public int solution(String myString, String pat) {
-        myString = myString.toLowerCase();
-        pat = pat.toLowerCase();
-        if (myString.contains(pat)) {
-            return 1;
-        }
-        return 0;
+    public String solution(String myString) {
+        return myString.toUpperCase();
     }
 }
 
@@ -18,7 +13,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution("AbCdEfG", "aBc"));
+        System.out.println(solution.solution("aBcDeFg"));
 
     }
 }
