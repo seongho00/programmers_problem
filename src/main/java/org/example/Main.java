7e0adf7db5b8ff74f81bf82f24e362574b85ee4a
seongho00 <tjdwns000927@gmail.com>
2025-05-13 10:31:32 +0900

    조건에 맞게 수열 변환하기1"
    "

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,30 +4,15 @@ package org.example;
 import java.util.*;
 
 class Solution {
-    public List<String> solution(String[] str_list) {
-        List<String> answer = new ArrayList<>();
-
-        if (str_list[0].equals("l")) {
-            return answer;
-        }
-
-        for (int i = 1, k = 0; i < str_list.length; i++, k++) {
-
-            if (str_list[k].equals("r")) {
-                for (int j = k + 1; j < str_list.length; j++) {
-                    answer.add(str_list[j]);
-                }
-                break;
-            } else if (str_list[i].equals("l")) {
-                for (int j = 0; j < i; j++) {
-                    answer.add(str_list[j]);
-                }
-                break;
-
+    public int[] solution(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= 50 && arr[i] % 2 == 0) {
+                arr[i] = arr[i] / 2;
+            } else if (arr[i] < 50 && arr[i] % 2 != 0) {
+                arr[i] = arr[i] * 2;
             }
         }
-
-        return answer;
+        return arr;
     }
 }
 
@@ -35,7 +20,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new String[]{"l", "r", "r", "r", "r"}));
+        System.out.println(solution.solution(new int[]{1, 2, 3, 100, 99, 98}));
 
     }
 }
