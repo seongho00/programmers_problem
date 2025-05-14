4eb6fc20ce2f5c9e37171e4055f88fad0c81b12c
seongho00 <tjdwns000927@gmail.com>
2025-05-14 15:03:02 +0900

    문자열 잘라서 정렬하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,22 +3,16 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 class Solution {
-    public List<Integer> solution(String myString) {
-        if (myString.startsWith("x")) {
-            myString = " " + myString;
-        }
-        if (myString.endsWith("x")) {
-            myString = myString + " ";
-        }
-        List<String> str_answer = new ArrayList<>(Arrays.asList(myString.split("x")));
-        List<Integer> int_answer = new ArrayList<>();
-        for (int i = 0; i < str_answer.size(); i++) {
-            int_answer.add(str_answer.get(i).trim().length());
-        }
-        return int_answer;
+    public List<String> solution(String myString) {
+
+        List<String> answer = new ArrayList<>(Arrays.asList(myString.split("x")));
+        answer.removeAll(Arrays.asList("", null));
+        Collections.sort(answer);
+        return answer;
     }
 }
 
@@ -26,7 +20,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution("oxooxoxxox"));
+        System.out.println(solution.solution("axbxcxdx"));
 
     }
 }
