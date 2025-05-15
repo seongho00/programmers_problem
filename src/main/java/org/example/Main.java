9aae577f235709f96d54f2c2d08f4a311f253abb
seongho00 <tjdwns000927@gmail.com>
2025-05-15 09:09:02 +0900

    빈 배열에 추가, 삭제하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,22 +7,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    public List<Integer> solution(int[] arr) {
+    public List<Integer> solution(int[] arr, boolean[] flag) {
         List<Integer> answer = new ArrayList<>();
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i]; j++) {
-                answer.add(arr[i]);
+        for (int i = 0; i < flag.length; i++) {
+            if (flag[i]) {
+                for (int j = 0; j < arr[i] * 2; j++) {
+                    answer.add(arr[i]);
+                }
+            } else {
+                for (int j = 0; j < arr[i]; j++) {
+                    answer.remove(answer.size() - 1);
+                }
             }
         }
         return answer;
+
     }
+
 }
 
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{5, 1, 4}));
+        System.out.println(solution.solution(new int[]{3, 2, 4, 1, 3}, new boolean[]{true, false, true, false, false}));
 
     }
 }
