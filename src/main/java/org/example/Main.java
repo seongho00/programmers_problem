b5984daa49281a81dabe9fa3dad0132ab9431eb8
seongho00 <tjdwns000927@gmail.com>
2025-05-15 09:03:51 +0900

    배열의 원소만큼 추가하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,25 +2,16 @@ package org.example;
 
 
 import java.util.Collections;
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    public int solution(String myString, String pat) {
-        int answer = 0;
-        for (int i = 0; i < myString.length() - pat.length() + 1; i++) {
-            boolean flag = true;
-            if (myString.charAt(i) == pat.charAt(0)) {
-                for (int j = 0; j < pat.length(); j++) {
-                    if (myString.charAt(i + j) != pat.charAt(j)) {
-                        flag = false;
-                        break;
-                    }
-                }
-                if (flag) {
-                    answer++;
-                }
+    public List<Integer> solution(int[] arr) {
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i]; j++) {
+                answer.add(arr[i]);
             }
         }
         return answer;
@@ -31,7 +22,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution("aaaa", "aa"));
+        System.out.println(solution.solution(new int[]{5, 1, 4}));
 
     }
 }
