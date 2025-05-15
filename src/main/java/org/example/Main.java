a0c4144952b3e368304cbcca50004abd45a77d87
seongho00 <tjdwns000927@gmail.com>
2025-05-15 09:14:07 +0900

    배열 만들기 6

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,30 +7,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    public List<Integer> solution(int[] arr, boolean[] flag) {
+    public List<Integer> solution(int[] arr) {
         List<Integer> answer = new ArrayList<>();
-        for (int i = 0; i < flag.length; i++) {
-            if (flag[i]) {
-                for (int j = 0; j < arr[i] * 2; j++) {
+        int i = 0;
+        while (i < arr.length) {
+            if (answer.isEmpty()) {
+                answer.add(arr[i]);
+            } else {
+                if (answer.get(answer.size() - 1) == arr[i]) {
+                    answer.remove(answer.size() - 1);
+                } else {
                     answer.add(arr[i]);
                 }
-            } else {
-                for (int j = 0; j < arr[i]; j++) {
-                    answer.remove(answer.size() - 1);
-                }
             }
+            i++;
+        }
+        if (answer.isEmpty()) {
+            answer.add(-1);
         }
         return answer;
-
     }
-
 }
 
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{3, 2, 4, 1, 3}, new boolean[]{true, false, true, false, false}));
+        System.out.println(solution.solution(new int[]{0, 1, 1, 1, 0}));
 
     }
 }
