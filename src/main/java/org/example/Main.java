2250012b3133b9893b20c1f71a853282bcbc418c
seongho00 <tjdwns000927@gmail.com>
2025-05-16 14:39:05 +0900

    세 개의 구분자"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,40 +1,23 @@
 package org.example;
 
 
-import java.util.Collections;
-import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class Solution {
-    public List<Integer> solution(int[] arr, int k) {
-        List<Integer> answer = new ArrayList<>();
+    public List<String> solution(String myStr) {
+        myStr = myStr.replace("a", "0").replace("b", "0").replace("c", "0");
+        List<String> before_answer = Arrays.asList(myStr.split("0"));
+        List<String> answer = new ArrayList<>();
 
-        for (int i = 0; i < arr.length; i++) {
-            boolean isPlus = true;
-            if (answer.isEmpty()) {
-                answer.add(arr[0]);
+        for (int i = 0; i < before_answer.size(); i++) {
+            if (!before_answer.get(i).trim().isEmpty()) {
+                answer.add(before_answer.get(i).trim());
             }
-
-            for (int j = 0; j < answer.size(); j++) {
-                if (answer.get(j) == arr[i]) {
-                    isPlus = false;
-                    break;
-                }
-
-            }
-            if (isPlus) {
-                answer.add(arr[i]);
-            }
-            if (answer.size() == k) {
-                break;
-            }
-
         }
-        if (answer.size() < k) {
-            for (int i = answer.size(); i < k; i++) {
-                answer.add(-1);
-            }
+        if (answer.isEmpty()) {
+            answer.add("EMPTY");
         }
 
         return answer;
@@ -45,7 +28,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{0, 1, 1, 1, 1, 1}, 3));
+        System.out.println(solution.solution("baconlettucetomato"));
 
     }
 }
