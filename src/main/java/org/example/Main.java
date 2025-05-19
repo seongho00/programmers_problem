81c189689bf5b396763ae093af1d19e89763a89a
seongho00 <tjdwns000927@gmail.com>
2025-05-19 09:10:21 +0900

    두 수의 합

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,29 +5,66 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 
+
 class Solution {
-    public List<Integer> solution(int[] arr) {
-        List<Integer> answer = new ArrayList<>();
-        if (arr.length == 1) {
-            for (int i : arr) {
-                answer.add(i);
+    public String solution(String a, String b) {
+        String answer = "";
+        int plus1 = 0;
+        boolean isUpper10 = false;
+        int length = Math.min(a.length(), b.length());
+        for (int i = 0; i < length; i++) {
+            int a_number = a.charAt(a.length() - 1 - i) - 48;
+            int b_number = b.charAt(b.length() - 1 - i) - 48;
+            plus1 = 0;
+            if (isUpper10) {
+                plus1 = 1;
             }
-            return answer;
+
+            if (a_number + b_number + plus1 >= 10) {
+                answer = (a_number + b_number + plus1) % 10 + answer;
+                isUpper10 = true;
+            } else {
+                answer = (a_number + b_number + plus1) + answer;
+                isUpper10 = false;
+            }
         }
 
-        int i = 0;
-        while (true) {
-            if (arr.length >= (int) Math.pow(2, i) && arr.length <= (int) Math.pow(2, i + 1)) {
-
-                break;
+        if (length != a.length()) {
+            for (int i = length; i < a.length(); i++) {
+                int a_number = a.charAt(a.length() - 1 - i) - 48;
+                plus1 = 0;
+                if (isUpper10) {
+                    plus1 = 1;
+                }
+                if (a_number + plus1 >= 10) {
+                    answer = (a_number + plus1) % 10 + answer;
+                    isUpper10 = true;
+                } else {
+                    answer = (a_number + plus1) + answer;
+                    isUpper10 = false;
+                }
             }
-            i++;
+        } else if (length != b.length()) {
+            for (int i = length; i < b.length(); i++) {
+                int b_number = b.charAt(b.length() - 1 - i) - 48;
+                plus1 = 0;
+                if (isUpper10) {
+                    plus1 = 1;
+                }
+                if (b_number + plus1 >= 10) {
+                    answer = (b_number + plus1) % 10 + answer;
+                    isUpper10 = true;
+                } else {
+                    answer = (b_number + plus1) + answer;
+                    isUpper10 = false;
+                }
+            }
+        } else if (isUpper10) {
+            answer = "1" + answer;
         }
-        for (int j = 0; j < arr.length; j++) {
-            answer.add(arr[j]);
-        }
-        for (int j = arr.length; j < (int) Math.pow(2, i + 1); j++) {
-            answer.add(0);
+
+        if (isUpper10 && answer.charAt(0) == '0') {
+            answer = "1" + answer;
         }
 
 
@@ -39,7 +76,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{1}));
+        System.out.println(solution.solution("582", "734"));
 
     }
 }
