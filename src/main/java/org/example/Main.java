4cc3135501b099279e41cb3ae29ec47178d0580d
seongho00 <tjdwns000927@gmail.com>
2025-05-19 09:13:42 +0900

    문자열 내 p와 y의 개수

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,68 +7,24 @@ import java.util.List;
 
 
 class Solution {
-    public String solution(String a, String b) {
-        String answer = "";
-        int plus1 = 0;
-        boolean isUpper10 = false;
-        int length = Math.min(a.length(), b.length());
-        for (int i = 0; i < length; i++) {
-            int a_number = a.charAt(a.length() - 1 - i) - 48;
-            int b_number = b.charAt(b.length() - 1 - i) - 48;
-            plus1 = 0;
-            if (isUpper10) {
-                plus1 = 1;
-            }
+    boolean solution(String s) {
+        s = s.toLowerCase();
+        int p_count = 0;
+        int y_count = 0;
 
-            if (a_number + b_number + plus1 >= 10) {
-                answer = (a_number + b_number + plus1) % 10 + answer;
-                isUpper10 = true;
-            } else {
-                answer = (a_number + b_number + plus1) + answer;
-                isUpper10 = false;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'p') {
+                p_count++;
+            }
+            if (s.charAt(i) == 'y') {
+                y_count++;
             }
         }
-
-        if (length != a.length()) {
-            for (int i = length; i < a.length(); i++) {
-                int a_number = a.charAt(a.length() - 1 - i) - 48;
-                plus1 = 0;
-                if (isUpper10) {
-                    plus1 = 1;
-                }
-                if (a_number + plus1 >= 10) {
-                    answer = (a_number + plus1) % 10 + answer;
-                    isUpper10 = true;
-                } else {
-                    answer = (a_number + plus1) + answer;
-                    isUpper10 = false;
-                }
-            }
-        } else if (length != b.length()) {
-            for (int i = length; i < b.length(); i++) {
-                int b_number = b.charAt(b.length() - 1 - i) - 48;
-                plus1 = 0;
-                if (isUpper10) {
-                    plus1 = 1;
-                }
-                if (b_number + plus1 >= 10) {
-                    answer = (b_number + plus1) % 10 + answer;
-                    isUpper10 = true;
-                } else {
-                    answer = (b_number + plus1) + answer;
-                    isUpper10 = false;
-                }
-            }
-        } else if (isUpper10) {
-            answer = "1" + answer;
+        if (p_count == y_count) {
+            return true;
         }
 
-        if (isUpper10 && answer.charAt(0) == '0') {
-            answer = "1" + answer;
-        }
-
-
-        return answer;
+        return false;
     }
 }
 
@@ -76,7 +32,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution("582", "734"));
+        System.out.println(solution.solution("pPoooyY"));
 
     }
 }
