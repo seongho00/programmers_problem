f301bfaf2397779f1b2692f799b839af3fcfa92e
seongho00 <tjdwns000927@gmail.com>
2025-05-19 16:14:58 +0900

    숫자의 표현

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,32 +1,28 @@
 package org.example;
 
 
+import java.util.Collections;
 import java.util.Arrays;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 
 class Solution {
-    public int solution(int[] A, int[] B) {
+    public int solution(int n) {
         int answer = 0;
-        List<Integer> A_list = new ArrayList<>();
-        List<Integer> B_list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            int sum = 0;
+            for (int j = i; j <= n; j++) {
+                sum += j;
 
-        for (int i = 0; i < A.length; i++) {
-            A_list.add(A[i]);
-            B_list.add(B[i]);
+                if (sum == n) {
+                    answer++;
+                    break;
+                } else if (sum > n) {
+                    break;
+                }
+            }
         }
-
-        int size = A_list.size();
-        Collections.sort(A_list);
-        Collections.sort(B_list);
-        Collections.reverse(B_list);
-
-        for (int i = 0; i < size; i++) {
-            answer += A_list.get(i) * B_list.get(i);
-        }
-
         return answer;
     }
 }
@@ -35,7 +31,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{1, 4, 2}, new int[]{5, 4, 4}));
+        System.out.println(solution.solution(15));
 
     }
 }
