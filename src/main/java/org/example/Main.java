45e4df857437f49ac84ed331708f859c9177d0a1
seongho00 <tjdwns000927@gmail.com>
2025-05-21 09:01:27 +0900

    그림 확대

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,13 +8,21 @@ import java.util.List;
 
 
 class Solution {
-    public int solution(String[] order) {
-        int answer = 0;
-        for (int i = 0; i < order.length; i++) {
-            if (order[i].contains("americano") || order[i].contains("anything")) {
-                answer += 4500;
-            } else if (order[i].contains("cafelatte")) {
-                answer += 5000;
+    public List<String> solution(String[] picture, int k) {
+        List<String> answer = new ArrayList<>();
+
+        for (int i = 0; i < picture.length; i++) {
+            String temp = "";
+            for (int j = 0; j < picture[i].length(); j++) {
+
+                for (int l = 0; l < k; l++) {
+                    temp += picture[i].charAt(j);
+                }
+
+            }
+            for (int j = 0; j < k; j++) {
+
+                answer.add(temp);
             }
         }
         return answer;
@@ -25,7 +33,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new String[]{"cafelatte", "americanoice", "hotcafelatte", " anything"}));
+        System.out.println(solution.solution(new String[]{".xx...xx.", "x..x.x..x", "x...x...x", ".x.....x.", "..x...x..", "...x.x...", "....x...."}, 2));
 
     }
 }
