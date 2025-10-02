8cab84eb5ed8175598833d969f70e0e401384846
seongho00 <tjdwns000927@gmail.com>
2025-10-02 10:28:42 +0900

    없는 숫자 더하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,18 +3,28 @@ package org.example;
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 class Solution {
-    public int solution(int[] absolutes, boolean[] signs) {
-
+    public int solution(int[] numbers) {
+        List<Integer> list = new ArrayList<>();
         int sum = 0;
-        for (int i = 0; i < absolutes.length; i++) {
-            if (signs[i]) {
-                sum += absolutes[i];
-            } else {
-                sum -= absolutes[i];
+        for (int number : numbers) {
+            list.add(number);
+        }
+
+        Collections.sort(list);
+
+        for (int i = 0; i < 10; i++) {
+            if (list.contains(i)) {
+                continue;
             }
+
+            sum += i;
+
         }
 
         return sum;
@@ -25,7 +35,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{4,7,12}, new boolean[]{true,false,true}));
+        System.out.println(solution.solution(new int[]{1, 2, 3, 4, 6, 7, 8, 0}));
 
     }
 }
