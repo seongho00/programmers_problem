9ba62129804dd44961fbd8286b8c744cb16ff796
seongho00 <tjdwns000927@gmail.com>
2025-10-02 12:04:04 +0900

    템플릿

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,30 +7,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-
 class Solution {
-    public List solution(int[] arr, int divisor) {
-
-        List<Integer> list = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
-        for (int number : arr) {
-            list.add(number);
+    public String solution(int n) {
+        String text = "수박";
+        // 짝수 일 때
+        if (n % 2 == 0) {
+            return text.repeat(n / 2);
         }
-
-        Collections.sort(list);
-        for (int i = 0; i < list.size(); i++) {
-
-            if (list.get(i) % divisor == 0) {
-                list2.add(list.get(i));
-            }
-
+        else {
+            return text.repeat(n / 2) + text.repeat(n / 2);
         }
-
-        if (list2.size() == 0) {
-            list2.add(-1);
-        }
-
-        return list2;
     }
 }
 
@@ -38,7 +24,10 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{5, 9, 7, 10}, 5));
+        int[] arr_int = new int[]{4, 3, 2, 1};
+        int[] arr_int2 = new int[]{-1, 0, 1};
+
+        System.out.println(solution.solution(3));
 
     }
 }
